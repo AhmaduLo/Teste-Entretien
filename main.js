45026b1e86cb7325bda4a2e58cf30478a5b2fa6a
AhmaduLo <ahmadubambagayelo@gmail.com>
2022-02-24 18:30:32 +0100

    add module

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const menuBurger = document.querySelector('.menu_burger')
+const btn_buy = document.querySelector('.btn_buy')
+const module_activation = document.querySelector('.module')
+const remove_module = document.querySelector('.remove_module')
 
 menuBurger.addEventListener('click', () => {
   menuBurger.classList.toggle('menu-anim')
+})
+
+btn_buy.addEventListener('click', () => {
+  module_activation.classList.toggle('active_module')
+})
+
+remove_module.addEventListener('click', () =>{
+  module_activation.classList.toggle('active_module')
 })
